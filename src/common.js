7cb9478627a40d7dc8174327c8df28f9f7387eff
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-07-07 16:16:19 +0000

    Angular 1.5 shit mix to stop from breaking :)

--- src/common.js
+++ src/common.js
@@ -10,10 +10,15 @@ angular.module('angularPayments')
 
     value = value || '';
 
-    value = value.replace(/\s/g, '');
-    _ref = value.split('/', 2);
-    month = _ref[0];
-    year = _ref[1];
+    // Note: tmp solution, value in Angular 1.5 has not a replace function since the date is invalid
+    try {
+      value = value.replace(/\s/g, '');
+      _ref = value.split('/', 2);
+      month = _ref[0];
+      year = _ref[1];
+    } catch (e) {
+
+    }
 
     if (year && year.length === 2 && /^\d+$/.test(year)) {
       prefix = (new Date()).getFullYear();
